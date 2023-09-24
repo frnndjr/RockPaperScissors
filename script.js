709e5a170253c315a763f2ed916fe955a1e47a08
Fernando <fp.velez520@gmail.com>
2023-09-23 21:02:32 -0400

    modified:   index.html modified:   script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//Gets the random generated computer choice
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    let computerChoice = choices[randomIndex];
+    return computerChoice;
+  }
