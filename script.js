41970de9abdac65bb8282c2c807dc9571a0fc9a5
Fernando <fp.velez520@gmail.com>
2023-09-23 21:27:19 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getComputerChoice() {
     let computerChoice = choices[randomIndex];
     return computerChoice;
   }
-  
+
   //Plays one round
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -23,3 +23,34 @@ function playRound(playerSelection, computerSelection) {
       return "You Lose! " + computerSelection + " beats " + playerSelection;
     }
   }
+
+  //5 rounds of gameplay
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+//This function is responsible for playing the game. It initializes two variables to keep track of the player's and computer's scores: playerScore and computerScore.
+
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Choose rock, paper, or scissors");
+    //This line prompts the player to enter their choice ("rock," "paper," or "scissors") using the prompt function.
+    const computerSelection = getComputerChoice();
+    // It calls the getComputerChoice() function to generate a random choice for the computer, which is stored in the computerSelection variable.
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+
+    if (result.includes("Win")) {
+      playerScore++;
+    } else if (result.includes("Lose")) {
+      computerScore++;
+    }
+  }
+
+  if (playerScore > computerScore) {
+    console.log("Congratulations! You win the game.");
+  } else if (playerScore < computerScore) {
+    console.log("Sorry, you lose the game.");
+  } else {
+    console.log("It's a tie! The game ends in a draw.");
+  }
+}
+game();
