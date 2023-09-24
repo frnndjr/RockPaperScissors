dd5aee911480ac670dfd4758aad98c4f11fed049
Fernando <fp.velez520@gmail.com>
2023-09-23 21:14:04 -0400

    The playRound function receives player and computer choices, determines the round's outcome

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //Gets the random generated computer choice
+//When you call the getComputerChoice() function, it will return a random choice for the computer
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
     let randomIndex = Math.floor(Math.random() * choices.length);
     let computerChoice = choices[randomIndex];
     return computerChoice;
+  }
+  
+  //Plays one round
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+  
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      return "You Win! " + playerSelection + " beats " + computerSelection;
+    } else {
+      return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
   }
